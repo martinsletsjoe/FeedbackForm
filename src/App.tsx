d3582513f52cf801a04172d49750f01a3d657806
martinsletsjoe <106916526+martinsletsjoe@users.noreply.github.com>
2024-03-26 19:20:10 +0100

    submission styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
 function App() {
@@ -23,20 +33,32 @@ function App() {
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <div>Tilbakemelding</div>
+        <div>Navn</div>
         <input value={inputName} onChange={handleNameInput} />
         <div>
           <Button colorScheme="blue" size="xs" type="submit">
-            yoyo
+            Submit
           </Button>
         </div>
       </div>
       <div>
-        {listFeedback.map((feedback, index) => (
-          <ul key={index}>
-            <li>{feedback}</li>
-          </ul>
-        ))}
+        <TableContainer>
+          <Table variant="striped" colorScheme="teal">
+            <Thead>
+              <Tr>
+                <Th>Submissions</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {listFeedback.map((feedback, index) => (
+                <Tr key={index}>
+                  <Td>{feedback}</Td>
+                  <Td isNumeric>{index + 1}</Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
       </div>
     </form>
   );
